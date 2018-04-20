4339290b2d515e859db9b5c914b5469bfcee866c
James Boggs <JBoggs2000@gmail.com>
2018-04-19 21:45:26 -0400

    Fixed backwardsness to complete part 2 of Ravenous

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import SearchBar from '../BusinessList/BusinessList';
-import BusinessList from '../SearchBar/SearchBar';
+import SearchBar from '../SearchBar/SearchBar';
+import BusinessList from '../BusinessList/BusinessList';
 
 const business = {
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
